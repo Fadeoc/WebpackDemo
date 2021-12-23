9df1451b05ac954ec76b77fd2a9c2670f0725240
zhengw <zhengw@hmgsoft.com>
2021-12-23 15:09:45 +0800

    12 Loader 链式调用

--- src/replaceTextLoader.js
+++ src/replaceTextLoader.js
@@ -1,9 +1,9 @@
-const replacer = function (content) {
-  return content.replace('内存中', '内存中并向你扔了一个软弱无力导致console.log被移除了').replace(/console\.log\(([^)]+)\);?/g, '');
+const replacer = function (sourceStr) {
+  return sourceStr.replace('内存中', '内存中并向你扔了一个软弱无力导致console.log被移除了').replace(/console\.log\(([^)]+)\);?/g, '');
 }
 
-const ReplaceTextLoader = function (content) {
-  return replacer(content)
+const ReplaceTextLoader = function (sourceStr) {
+  return replacer(sourceStr)
 }
 
 module.exports = ReplaceTextLoader;
